301804a866f77b5f099e8f56005531b73feae19e
4 No 7 <yourmom@gmail.com>
2025-05-07 19:23:54 +0300

    long time no push

---
+++ FRAMERIPPER/src/deauth.cpp
@@ -0,0 +1,5 @@
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include "types.h"
+#include "deauth.h"
+#include "definitions.h"
---
+++ FRAMERIPPER/src/main.cpp
@@ -0,0 +1,6 @@
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include "types.h"
+#include "web_interface.h"
+#include "deauth.h"
+#include "definitions.h"
